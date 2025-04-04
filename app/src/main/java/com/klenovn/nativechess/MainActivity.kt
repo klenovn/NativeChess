f31defb029f714b4f1071d9c79437a6c56dc3f36
Nikita Klenov <146123813+klenovn@users.noreply.github.com>
2025-04-04 16:40:13 +0300

    feat(navigation): add base navigation graph and host

--- app/src/main/java/com/klenovn/nativechess/MainActivity.kt
+++ app/src/main/java/com/klenovn/nativechess/MainActivity.kt
@@ -2,4 +2,4 @@ package com.klenovn.nativechess
 
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity()
+class MainActivity : AppCompatActivity(R.layout.activity_main)
